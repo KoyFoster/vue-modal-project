345689539f12effd3ea0e335a644325f3bec4e89
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-04-08 13:41:10 -0500

    Template refs intro

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "[This is a title]",
+    };
+  },
+  methods: {
+    handleClick() {
+      const nameRef = this.$refs.name;
+      console.warn({ nameRef });
+      nameRef.classList.add("active");
+      nameRef.focus();
+    },
+  },
+  computed: {},
+};
 </script>
 
+<!-- Is global to the entire page, not just this component file -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +33,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
