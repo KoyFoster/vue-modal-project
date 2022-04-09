bd778319b3f50203346be32ab2e58ad07a119233
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-04-08 21:37:11 -0500

    New project setup

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,15 @@
 <template>
-  <h1>{{ title }}</h1>
   <p>Welcome</p>
-  <br />
-  <teleport to="#header" v-if="showModal1">
-    <ModalWindow theme="" @close="toggleModal1">
-      <template v-slot:links>
-        <a href="#">sign up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Giveaway!</h1>
-      <p>Half price!</p>
-    </ModalWindow>
-  </teleport>
-
-  <teleport to=".footer" v-if="showModal2">
-    <ModalWindow theme="sales" @close="toggleModal2">
-      <h1>Coupons!</h1>
-      <p>Half price!</p>
-    </ModalWindow>
-  </teleport>
-  <br />
-
-  <button @click.alt="toggleModal2" @click.exact="toggleModal1">
-    open modal (alt for modal 2)
-  </button>
-
-  <!-- <input type="text" ref="name" />
-  <button @click="handleClick">click me</button> -->
 </template>
 
 <script>
-// import
-import ModalWindow from "./components/modal-window.vue";
-
 export default {
   name: "App",
-  components: { ModalWindow },
+  components: {},
   data() {
-    return {
-      showModal1: false,
-      showModal2: false,
-    };
+    return {};
   },
-  methods: {
-    toggleModal1() {
-      this.showModal1 = !this.showModal1;
-    },
-    toggleModal2() {
-      this.showModal2 = !this.showModal2;
-    },
-  },
+  methods: {},
   computed: {},
 };
 </script>
